481ecb7f7c62aad533051e718c46b2ee6719d7bc
Bao NguyenNgoGia <bao.nguyen.04082001@hcmut.edu.vn>
2023-04-10 21:17:15 +0700

    ! initial commit for service server

---
+++ Services-server/Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Service.Models
+{
+    public class Users
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("userId")]
+        public string userId { get; set; } = null!;
+
+        public string? nickName { get; set; }
+
+        public string email { get; set; } = null!;
+
+        public decimal currency { get; set; } = 0;
+
+        public List<int>? cards { get; set; }
+
+    }
+}
---
+++ Services-server/Program.cs
@@ -0,0 +1,39 @@
+using Service.Models;
+using Service.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Todo
+builder.Services.Configure<UsersDatabaseSettings>(
+    builder.Configuration.GetSection("MythicEmpire"));
+builder.Services.AddSingleton<UsersService>();
+
+
+
+
+// End todo
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
