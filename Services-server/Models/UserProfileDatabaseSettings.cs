838e5178a91b4361009eb2ba0613c86c31d0380b
Bao NguyenNgoGia <bao.nguyen.04082001@hcmut.edu.vn>
2023-04-10 21:17:15 +0700

    ! initial commit for service server

---
+++ Services-server/Models/UserProfileDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Service.Models
+{
+    public class UserProfileDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string UserProfileCollectionName { get; set; } = null!;
+    }
+}
